866efeba3080bd6cda1d0327db05edb47758355e
yuliatos12 <125970340+yuliatos12@users.noreply.github.com>
2023-08-17 14:40:16 +0300

    +

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,49 @@
+let currentPanel = 1;
+let totalPanels = 0;
+let autoPlay = true;
+let timePassed = 0;
+let timeToChange = 3;
+
+function autoAdvance() {
+  if (timePassed == timeToChange) {
+    timePassed = 0;
+    if (currentPanel == totalPanels) {
+      currentPanel = 0;
+    }
+    if (autoPlay == true) {
+      $(
+        ".marquee_nav a.marquee_nav_item:nth-child(" + (currentPanel + 1) + ")"
+      ).trigger("click");
+    }
+  } else {
+    timePassed += 1;
+  }
+  $(".timePassed").html("timePassed = " + timePassed);
+  $(".autoPlay").html("autoPlay = " + autoPlay);
+}
+
 $(document).ready(function () {
+  // debug
+
+  $(".autoPlay").html("autoPlay = " + autoPlay);
+  $(".timePassed").html("timePassed = " + timePassed);
+  $(".timeToChange").html("timeToChange = " + timeToChange);
+  $(".currentPanel").html("currentPanel = " + currentPanel);
+  //
+  setInterval(autoAdvance, 1000);
+
+  $('.marquee_container').hover(
+function() {
+autoPlay = false;
+$(this).removeClass('autoplay');
+},
+function(){
+autoPlay = true;
+timePassed = 0;
+$(this).addClass('autoplay');
+}
+  );
+
   $("img.marquee_panel_photo").each(function (index) {
     const photoWidth = $(".marquee_container").width();
     const photoPosition = index * photoWidth;
@@ -17,6 +62,8 @@ $(document).ready(function () {
 
   $(".marquee_panels .marquee_panel").each(function (index) {
     $(".marquee_nav").append('<a class="marquee_nav_item"></a>');
+    totalPanels = index + 1;
+    $(".totalPanels").html("totalPanels = " + totalPanels);
   });
 
   $(".marquee_nav a.marquee_nav_item").click(function () {
@@ -27,6 +74,9 @@ $(document).ready(function () {
     const marqueeWidth = $(".marquee_container").width();
     const distanceToMove = marqueeWidth * -1;
     const newPhotoPosition = navClicked * distanceToMove + "px";
+
+    currentPanel = navClicked + 1;
+    $(".currentPanel").html("currentPanel = " + currentPanel);
 
     $(".marquee_photos").animate({ left: newPhotoPosition }, 1000);
   });
